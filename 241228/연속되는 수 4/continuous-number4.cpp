0eecc27bf2339fcd0b7c72e7e6ff563865b6c835
downfa11 <124482256+downfa11@users.noreply.github.com>
2024-12-28 00:52:34 +0900

    Title: 연속되는 수 4, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241228/연속되는 수 4/continuous-number4.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> v;
+
+int calcLongestSequence(int k){
+    int prev=0, length=0;
+
+    for(int i=k;i<v.size();i++){
+        if(v[i]==prev)
+            return length;
+
+        length++;
+        prev = v[i];
+    }
+    
+    return length;
+}
+
+int main() {
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        int a;
+        cin>> a;
+        v.push_back(a);
+    }
+
+    sort(v.begin(), v.end(), less<int>());
+
+    int result=0;
+    for(int i=0; i<v.size();i++)
+        result = max(result,calcLongestSequence(i));
+
+    cout << result << endl;
+    return 0;
+}
